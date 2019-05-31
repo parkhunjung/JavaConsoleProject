8a3066bbe73aa1b53d5b99223c49ff0ea2652b9b
parkhunjung <qkrgjswjd100@daum.net>
2019-05-31 11:17:16 +0900

    commit

--- kr/co/sist/cinema/post/manager/PostUI.java
+++
@@ -1,5 +0,0 @@
-package kr.co.sist.cinema.post.manager;
-
-public class PostUI {
-
-}
--- kr/co/sist/cinema/reservation/Ex.java
+++
@@ -1,9 +0,0 @@
-package kr.co.sist.cinema.reservation;
-
-public class Ex {
-
-	public static void main(String[] args) {
-		CinemaScheduleDataInsert c = new CinemaScheduleDataInsert();
-		c.timeInsert();
-	}
-}
